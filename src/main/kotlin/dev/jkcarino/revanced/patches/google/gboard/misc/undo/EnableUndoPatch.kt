6804fd725764ee59690e9cabe2adc83ecbf8c2df
Jhon Kenneth Cariño <6307355+jkennethcarino@users.noreply.github.com>
2024-04-17 00:14:56 +0800

    feat(Gboard): Add `Enable Undo feature for Gboard` patch

---
+++ src/main/kotlin/dev/jkcarino/revanced/patches/google/gboard/misc/undo/EnableUndoPatch.kt
@@ -0,0 +1,36 @@
+package dev.jkcarino.revanced.patches.google.gboard.misc.undo
+
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstructions
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+import dev.jkcarino.revanced.patches.all.signature.pmshook.BypassSignatureChecksPatch
+import dev.jkcarino.revanced.patches.google.gboard.misc.undo.fingerprints.UndoAccessPointFingerprint
+import dev.jkcarino.revanced.util.exception
+
+@Patch(
+    name = "Enable Undo feature for Gboard",
+    description = "Enables undo feature to quickly undo or correct typing mistakes. " +
+        "This requires the original, unmodified app to work properly.",
+    dependencies = [
+        BypassSignatureChecksPatch::class,
+    ],
+    compatiblePackages = [CompatiblePackage("com.google.android.inputmethod.latin")],
+    use = false
+)
+@Suppress("unused")
+object EnableUndoPatch : BytecodePatch(
+    setOf(UndoAccessPointFingerprint)
+) {
+    override fun execute(context: BytecodeContext) {
+        UndoAccessPointFingerprint.result?.let { result ->
+            val isEnabledIndex = result.scanResult.patternScanResult!!.endIndex
+
+            result.mutableMethod.replaceInstructions(
+                index = isEnabledIndex,
+                smaliInstructions = "const/4 v1, 0x1"
+            )
+        } ?: throw UndoAccessPointFingerprint.exception
+    }
+}
