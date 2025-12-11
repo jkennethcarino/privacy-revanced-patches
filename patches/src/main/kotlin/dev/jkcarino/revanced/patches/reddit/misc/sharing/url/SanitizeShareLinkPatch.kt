0bcdcb86d387073e072abf37cf336926eedbae2f
Jhon Kenneth Cariño <6307355+jkennethcarino@users.noreply.github.com>
2025-12-11 12:57:12 +0800

    feat(Reddit): Add `Sanitize share links` patch

---
+++ patches/src/main/kotlin/dev/jkcarino/revanced/patches/reddit/misc/sharing/url/SanitizeShareLinkPatch.kt
@@ -0,0 +1,30 @@
+package dev.jkcarino.revanced.patches.reddit.misc.sharing.url
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.patch.bytecodePatch
+
+@Suppress("unused")
+val sanitizeShareLinkPatch = bytecodePatch(
+    name = "Sanitize share links",
+    description = "Unshortens and removes the tracking query parameters from shared links.",
+    use = true
+) {
+    compatibleWith("com.reddit.frontpage")
+
+    execute {
+        createShareLinkFingerprint.method.addInstruction(
+            index = 0,
+            smaliInstructions = "return-object p0"
+        )
+
+        val generateShareLinkMethod = generateShareLinkFingerprint.method
+        val getShortUrl = navigate(generateShareLinkMethod)
+            .to(generateShareLinkFingerprint.patternMatch!!.endIndex - 1)
+            .stop()
+
+        getShortUrl.addInstruction(
+            index = 0,
+            smaliInstructions = "return-object p1"
+        )
+    }
+}
