3603bf8bd1553e6db2d8e097c00c7c28c23beff3
Jhon Kenneth Cariño <6307355+jkennethcarino@users.noreply.github.com>
2026-02-15 18:14:59 +0800

    feat(Reddit): Add `Hide Ask button from search bar` patch

---
+++ patches/src/main/kotlin/dev/jkcarino/revanced/patches/reddit/layout/search/ask/Fingerprint.kt
@@ -0,0 +1,25 @@
+package dev.jkcarino.revanced.patches.reddit.layout.search.ask
+
+import app.revanced.patcher.fingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
+
+internal val staticConstructorFingerprint = fingerprint {
+    accessFlags(AccessFlags.STATIC, AccessFlags.CONSTRUCTOR)
+    strings(
+        "isSearchBarAskButtonHoldoutEnabled",
+        "isSearchBarAskButtonHoldoutEnabled()Z"
+    )
+}
+
+internal val isSearchBarAskButtonHoldoutEnabledFingerprint = fingerprint {
+    returns("Z")
+    parameters()
+    opcodes(
+        Opcode.SGET_OBJECT,
+        Opcode.AGET_OBJECT,
+        Opcode.IGET_OBJECT,
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.MOVE_RESULT_OBJECT,
+    )
+}
